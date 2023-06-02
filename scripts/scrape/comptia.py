b3a08bd8520b2c14c46f5720aa430177f96ba949
bcrpntr <44307215+bcrpntr@users.noreply.github.com>
2023-06-01 20:45:59 -0500

    Update comptia.py

--- scripts/scrape/comptia.py
+++ scripts/scrape/comptia.py
@@ -1,3 +1,16 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+import json
+from github import Github
+import re
+
+urls = {
+    'https://www.comptia.org/continuing-education/choose/renewing-with-multiple-activities/additional-comptia-certifications': 'CompTIA'
+}
+
+import re  # Add this import at the top of your script
+
 def scrape_comptia(url):
     r = requests.get(url)
     r.raise_for_status()
@@ -37,3 +50,30 @@ def scrape_comptia(url):
                         "CEUs Granted": ceus_granted
                     }
     return data
+
+def write_json_file(file_path, data, repo):
+    file_name = os.path.basename(file_path)
+    directory = os.path.dirname(file_path)
+    try:
+        repo.get_contents(file_path)
+    except:
+        repo.create_file(f"{directory}/{file_name}", f"Create file {file_name}", json.dumps(data, indent=4))
+
+def create_certification_folders(vendor, certifications, repo):
+    for certification, ceus in certifications.items():
+        if ceus == 'N/A' or ceus == '':
+            continue
+        file_path = os.path.join(vendor, certification, 'data.json')
+        write_json_file(file_path, {certification: ceus}, repo)
+
+def main():
+    github_token = os.getenv('TOKEN')
+    g = Github(github_token)
+    repo = g.get_repo("bcrpntr/it-certifications")
+
+    for url, vendor in urls.items():
+        data = scrape_comptia(url)
+        create_certification_folders(vendor, data, repo)
+
+if __name__ == '__main__':
+    main()
