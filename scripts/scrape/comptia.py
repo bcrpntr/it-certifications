9b44e22c471433a2998950a8bdaf45a4015ebe29
bcrpntr <44307215+bcrpntr@users.noreply.github.com>
2023-06-01 20:50:20 -0500

    Update comptia.py

--- scripts/scrape/comptia.py
+++ scripts/scrape/comptia.py
@@ -21,33 +21,32 @@ def scrape_comptia(url):
     data = {}
     for item in items:
         certification_element = item.find(class_='title')
-        ceus_element = item.find('p')  # Fetch the 'p' tag content first
 
-        if certification_element and ceus_element:
+        if certification_element:
             certifications = certification_element.get_text(strip=True).split(',')
-            
-            # Splitting on '–' since this is the character between required certification and CEUs granted
-            content_parts = ceus_element.get_text(strip=True).split('–', 1)
 
-            required_certification = content_parts[0].strip() if len(content_parts) > 1 else "N/A"
+            # Get the sibling 'div' element
+            sibling_div = item.find_next_sibling('div')
+            if sibling_div:
+                content_parts = sibling_div.get_text(strip=True).split('–', 1)
+                
+                required_certification = content_parts[0].strip() if len(content_parts) > 1 else "N/A"
 
-            # Within 'p' tag content, fetch 'strong' tag for CEUs
-            ceus_content = ceus_element.find('strong').get_text(strip=True) if ceus_element.find('strong') else 'N/A'
+                # Extract only the numeric part if "CEUs" present in the string
+                ceus_granted_search = re.search(r'(\d+)\s*CEUs?', content_parts[-1], re.IGNORECASE)
+                ceus_granted = ceus_granted_search.group(1) if ceus_granted_search else 'N/A'
 
-            # Extract only the numeric part of ceus_content if "CEUs" present in the string
-            ceus_granted_search = re.search(r'(\d+)\s*CEUs?', ceus_content, re.IGNORECASE)
-            ceus_granted = ceus_granted_search.group(1) if ceus_granted_search else 'N/A'
+                for certification in certifications:
+                    certification = certification.strip()  # Remove leading and trailing whitespace
 
-            for certification in certifications:
-                certification = certification.strip()  # Remove leading and trailing whitespace
-
-                # Ensure ceus_granted is not 'N/A' or an empty string, and is a digit
-                if ceus_granted not in {'N/A', ''} and ceus_granted.isdigit():
-                    data[certification] = {
-                        "Required Certification": required_certification,
-                        "CEUs Granted": ceus_granted
-                    }
+                    # Ensure ceus_granted is not 'N/A' or an empty string, and is a digit
+                    if ceus_granted not in {'N/A', ''} and ceus_granted.isdigit():
+                        data[certification] = {
+                            "Required Certification": required_certification,
+                            "CEUs Granted": ceus_granted
+                        }
     return data
+
 
 def write_json_file(file_path, data, repo):
     file_name = os.path.basename(file_path)
